704b8a9f9ec9e34ce33ffa9cff20653ec8d79234
Mateo Álvarez <maalvarezmu@unal.edu.co>
2023-04-02 12:10:45 -0500

    Creación de clases

---
+++ src/main/java/comunicacion/Periodico.java
@@ -0,0 +1,51 @@
+package comunicacion;
+
+public class Periodico extends Escrito{
+	private String fecha;
+	private String primicia;
+	private String interpretacion;
+	
+	public Periodico(String origen, String titulo, String autor, int paginas, String fecha, String primicia, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.fecha=fecha;
+		this.primicia=primicia;
+		this.interpretacion=interpretacion;
+	}
+	
+	public String getFecha() {
+		return this.fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public String getPrimicia() {
+		return this.primicia;
+	}
+	public void setPrimicia(String primicia) {
+		this.primicia = primicia;
+	}
+	public String interpretacion() {
+		return this.interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	public String toString() {
+		String retu="";
+		retu+=this.resumen();
+		retu+="\n"+this.fecha;
+		retu+="\n"+this.primicia;
+		return retu;
+	}
+	
+	public int palabrasTotales() {
+		return palabrasTotales(10);
+	}
+	
+	public int palabrasTotales(int numero) {
+		return this.getPaginas()*numero;
+	}
+	
+	
+}
